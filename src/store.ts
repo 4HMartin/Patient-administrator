97579381dd7db79ce9b210aaaac86929622d344e
Heber <parzival.html@gmail.com>
2025-08-28 20:44:29 +0200

    Defining actions in the Zustand's store.

--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,15 @@
 import {create} from 'zustand'
-import type { Patient } from './types'
+import type { DraftPatient, Patient } from './types'
 
+/** The state and its actions must be defined within the state type */
 type PatientState = {
     patients: Patient[]
+    addPatient: (data: DraftPatient) => void
 }
 
 export const usePatientStore = create<PatientState>(() => ({
-    patients: []
+    patients: [],
+    addPatient(data) {
+        console.log(data)
+    },
 }))
