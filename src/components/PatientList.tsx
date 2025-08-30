e71382c398a763256bf05cb167040a5af449ce4f
Heber <parzival.html@gmail.com>
2025-08-30 14:42:39 +0200

    Developing the list of patients.

--- src/components/PatientList.tsx
+++ src/components/PatientList.tsx
@@ -1,11 +1,37 @@
 import { usePatientStore } from "../store"
+import PatientDetails from "./PatientDetails"
 
 export default function PatientList(){
 
     const patients = usePatientStore(state => state.patients)
-    console.log(patients)
 
     return (
-        <h1>Patient List</h1>
+        <div className="md:w-1/2 md:h-screen lg:w-3/5 overflow-y-scroll">
+            {patients.length ? (
+                <>
+                    <h2 className="font-black text-3xl text-center">List of patients</h2>
+                    <p className="text-xl mt-5 mb-10 text-center">
+                        Manage your {''}
+                        <span className="text-indigo-600 font-bold">Patients and Dates</span>
+                    </p>
+                    {patients.map( patient => (
+                        <PatientDetails
+                            key={patient.id}
+                            patient={patient}
+                        />
+                    ))}    
+                </>
+            ): (
+                <>
+                    <h2 className="font-black text-3xl text-center">
+                        There are no patients
+                    </h2>
+                    <p className="text-xl mt-5 mb-10 text-center">
+                        Start by adding patients and {''}
+                        <span className="text-indigo-600 font-bold">they will appear here</span>
+                    </p>
+                </>
+            )}
+        </div>
     )
 }
