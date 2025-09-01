1f114d8473cb2d29c86cf8a03d96b83ffefcdc8f
Heber <parzival.html@gmail.com>
2025-09-01 18:01:13 +0200

    Showing the patient's information.

--- src/components/PatientDetails.tsx
+++ src/components/PatientDetails.tsx
@@ -1,4 +1,5 @@
 import type { Patient } from "../types"
+import PatientDetailsItem from "./PatientDetailsItem"
 
 type PatientDetailsProps = {
     patient: Patient
@@ -6,7 +7,13 @@ type PatientDetailsProps = {
 
 export default function PatientDetails({patient} : PatientDetailsProps){
     return (
-        <>
-        </>
+        <div className="mx-5 my-10 px-5 py-10 bg-white shadow-md rounded-xl">
+            <PatientDetailsItem label={'ID'} data={patient.id} />
+            <PatientDetailsItem label={'Name'} data={patient.name} />
+            <PatientDetailsItem label={'Owner'} data={patient.caretaker} />
+            <PatientDetailsItem label={'Email'} data={patient.email} />
+            <PatientDetailsItem label={'Date of registration'} data={patient.date.toString()} />
+            <PatientDetailsItem label={'Symptoms'} data={patient.symptoms} />
+        </div>
     )
 }
